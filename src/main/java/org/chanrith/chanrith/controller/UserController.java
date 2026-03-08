af0441e03e6fad77ba52c67b18e4528d5c9d89b6
Roung Chanrith <chanrithroung2@gmail.com>
2026-03-08 22:20:07 +0700

    improve code

--- src/main/java/org/chanrith/chanrith/controller/UserController.java
+++ src/main/java/org/chanrith/chanrith/controller/UserController.java
@@ -9,18 +9,10 @@ import java.util.List;
 @RequestMapping("/api/v1/user")
 public class UserController {
     List<User> userList = new ArrayList<>();
-
-
     ///  this method use for check email is already exist or not
     /// if exist @return true, otherwise return false;
     boolean isExistEmail(String requestEmail) {
-        boolean validEmail = true;
-        for (User user : userList) {
-            if(user.getEmail().equals(requestEmail)) {
-                return true;
-            }
-        }
-        return false;
+       return userList.stream().anyMatch(user -> user.getEmail().equals(requestEmail));
     }
 
     @GetMapping
@@ -31,23 +23,17 @@ public class UserController {
     // after follow the best practice I will code for check username or email
     @PostMapping
     String createUser(@RequestBody User userRequest) {
-       if(isExistEmail(userRequest.getEmail())) {
+       if(isExistEmail(userRequest.getEmail()))
           return "Failed to create user! this email is already used!";
-       } else {
-           Integer newId;
-           if(userList.toArray().length == 0) {
-               newId = 1;
-           } else {
-               newId = userList.get(userList.toArray().length - 1).getId() + 1;
-           }
-           User newUser = new User(
-                   newId,
-                   userRequest.getUsername(),
-                   userRequest.getEmail()
-           );
-           userList.add(newUser);
-           return "User saved!";
-       }
+       Integer newId;
+       if(userList.toArray().length == 0) newId = 1;
+       newId = userList.get(userList.toArray().length - 1).getId() + 1;
+       User newUser = new User(
+               newId,
+               userRequest.getUsername(),
+               userRequest.getEmail()
+       );
+       userList.add(newUser);
+       return "User saved!";
     }
-
 }
